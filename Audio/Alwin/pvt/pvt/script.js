1ef3fdb2d81bdbefa86d189f8ef266d9cf744d26
MentorRSC <mentor2hubrsc@gmail.com>
2026-04-10 17:48:39 +0530

    Update script.js

--- Audio/Alwin/pvt/pvt/script.js
+++ Audio/Alwin/pvt/pvt/script.js
@@ -1,6 +1,7 @@
 // ========== CONFIGURATION ==========
 // REPLACE THESE WITH YOUR ACTUAL GOOGLE SHEETS IDs
 const SHEET1_ID = "1QP_vL_AbyNcI6GX3Ki_kQHKKJ9Lht3_MUFyMWslkNKU"; // Router sheet
+const SHEET2_ID = "1cPH-nLX5-8TSX29qKr7FE17nHvIl0RF9tv7GTWUCwNE"; // Color themes
 const SHEET3_ID = "1cPH-nLX5-8TSX29qKr7FE17nHvIl0RF9tv7GTWUCwNE"; // Color themes
 const SHEET4_ID = "12y7yvyP9geaEzjH4Qo5tgA9HEqihpMp2ULp8n_QEYhA"; // Card styles
 const SHEET5_ID = "1lOtB6gISk6K7A8-t5LjcCRh-SswE7RBxa9HdIXPpv_0"; // Headings
